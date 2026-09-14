5d654890d405b649aae533013df20fef0c299fb7
benja <b.ferrazzuolo.niza@itsv.edu.ar>
2026-09-14 23:33:55 +0000

    se agregaron los html home y base dentro de la ccarpeta templates

--- streaming/views.py
+++ streaming/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    """Vista para la landing page / página de inicio pública"""
+    return render(request, 'home.html')
