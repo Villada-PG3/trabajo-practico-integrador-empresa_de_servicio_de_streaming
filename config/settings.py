dd53b9a8f616be78e6e3e19d1b5d21a5cc2107b3
Ezequiel <ezemaxycorty@gmail.com>
2026-09-14 14:51:33 +0000

    actualizo las 3 apps para trabajar solo con una, ya echas las migraciones

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pagos',
     'streaming',
-    'user',
     'phonenumber_field',
 ]
 
@@ -102,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'user.Usuario'
+AUTH_USER_MODEL = 'streaming.Usuario'
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
--- streaming/models.py
+++ streaming/models.py
@@ -1,11 +1,15 @@
 from django.db.models import *
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Pais(models.Model):
     nombre = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.nombre
+        return self.nombre 
 
 
 class TipoTitulo(models.Model):
@@ -157,7 +161,7 @@ class CalidadIdiomaContenido(models.Model):
 
 class PerfilTitulo(models.Model):
     # Historial de titulos vistos por un perfil, con su calificacion (para recomendaciones)
-    perfil = models.ForeignKey('user.Perfil', on_delete=models.CASCADE, related_name='perfil_titulos')
+    perfil = models.ForeignKey('Perfil', on_delete=models.CASCADE, related_name='perfil_titulos')
     titulo = models.ForeignKey(Titulo, on_delete=models.CASCADE, related_name='perfil_titulos')
     calificacion = models.ForeignKey(
         Calificacion, on_delete=models.SET_NULL, null=True, blank=True, related_name='perfil_titulos'
@@ -178,10 +182,163 @@ class Porver(models.Model):
 
 class PerfilContenido(models.Model):
     # Cuanto vio un perfil un determinado contenido
-    perfil = models.ForeignKey('user.Perfil', on_delete=models.CASCADE, related_name='perfil_contenidos')
+    perfil = models.ForeignKey('Perfil', on_delete=models.CASCADE, related_name='perfil_contenidos')
     contenido = models.ForeignKey(Contenido, on_delete=models.CASCADE, related_name='perfil_contenidos')
     segundos_vistos = models.IntegerField(default=0)
     ultima_visualizacion = models.DateTimeField()
 
     def __str__(self):
         return f'{self.perfil} - {self.contenido}'
+
+
+class MarcaTarjeta(models.Model):
+    nombre = models.CharField(max_length=100)
+    numero_asignado = models.IntegerField()
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class MetodoPago(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Tarjeta(models.Model):
+    numero = models.CharField(max_length=30)
+    nombre_titular = models.CharField(max_length=150)
+    fecha_vencimiento = models.DateField()
+    marca_tarjeta = models.ForeignKey(MarcaTarjeta, on_delete=models.PROTECT, related_name='tarjetas')
+    metodo_pago = models.ForeignKey(MetodoPago, on_delete=models.PROTECT, related_name='tarjetas')
+    habilitado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.marca_tarjeta} **** {self.numero[-4:]}'
+    """
+    # Es muy posible que esto no se deberia guardar, en cuyo caso:
+    # ultimos 4 nums para reconocerla
+    numero = models.CharField(max_length=4)
+    marca_tarjeta = models.ForeignKey(MarcaTarjeta, on_delete=models.PROTECT, related_name='tarjetas')
+    metodo_pago = models.ForeignKey(MetodoPago, on_delete=models.PROTECT, related_name='tarjetas')
+    habilitado = models.BooleanField(default=True)
+    """
+
+
+class Plan(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    cantidad_pantallas_simultaneas = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class PlanCalidad(models.Model):
+    # Soluciona la n:m entre Plan y Calidad (streaming)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE, related_name='plan_calidades')
+    calidad = models.ForeignKey('streaming.Calidad', on_delete=models.CASCADE, related_name='plan_calidades')
+
+    class Meta:
+        verbose_name = 'Calidad del plan'
+        verbose_name_plural = 'Calidades del plan'
+
+    def __str__(self):
+        return f'{self.plan} - {self.calidad}'
+
+
+class Precio(models.Model):
+    precio = models.FloatField()
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField(blank=True, null=True)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE, related_name='precios')
+
+    def __str__(self):
+        return f'{self.plan} - {self.precio}'
+
+
+class Suscripcion(models.Model):
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='suscripciones')
+    plan = models.ForeignKey(Plan, on_delete=models.PROTECT, related_name='suscripciones')
+    fecha_inscripcion = models.DateField()
+    fecha_vigencia = models.DateField(blank=True, null=True)
+    fecha_cancelacion = models.DateField(blank=True, null=True)
+    habilitado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.usuario} - {self.plan}'
+
+
+class EstadoCobro(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Cobro(models.Model):
+    num_factura = models.IntegerField()
+    suscripcion = models.ForeignKey(Suscripcion, on_delete=models.PROTECT, related_name='cobros')
+    fecha_cobro = models.DateField()
+    estado_cobro = models.ForeignKey(EstadoCobro, on_delete=models.PROTECT, related_name='cobros')
+    # se copia de la suscripcion al momento del cobro, para dejar historial
+    tarjeta = models.ForeignKey(Tarjeta, on_delete=models.PROTECT, related_name='cobros')
+    numero_autorizacion = models.CharField(max_length=100, blank=True, null=True)
+    monto = models.FloatField()
+
+    def __str__(self):
+        return f'Factura {self.num_factura} - {self.suscripcion}' 
+
+     
+class Imagen(Model):
+    id = BigAutoField(primary_key=True)
+    nombre = CharField(max_length=100)
+    imagen = FileField() # hay que ver despues como se implementa
+    titulo = ForeignKey(Titulo, on_delete=CASCADE)
+    
+    def __str__(self):
+        return self.nombre
+
+class Usuario(AbstractUser):
+    id = BigAutoField(primary_key=True)
+    telefono = PhoneNumberField(blank=True)
+    pais = ForeignKey(Pais, on_delete=CASCADE)
+    idioma = ForeignKey(Idioma, on_delete=CASCADE)
+    # hay que ver despues la cuestion con tarjeta
+    
+    def __str__(self):
+        return self.username
+    
+class TipoPerfil(Model):
+    id = BigAutoField(primary_key=True)
+    nombre = CharField(max_length=100)
+    edad_max = IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    
+    def __str__(self):
+        return self.nombre
+    
+class Perfil(Model):
+    id = BigAutoField(primary_key=True)
+    nombre = CharField(max_length=100)
+    imagen = ForeignKey(Imagen, on_delete=CASCADE)
+    tipo_perfil = ForeignKey(TipoPerfil, on_delete=CASCADE)
+    usuario = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Registro(Model):
+    id = BigAutoField(primary_key=True)
+    token_disp = CharField(max_length=100)
+    os = CharField(max_length=100)
+    ultima_conexion_fecha = DateField()
+    ultima_conexion_hora = TimeField()
+    estado = BooleanField()
+    usuario = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    
+    def __str__(self):
+        return f'{self.usuario} - {self.os}'    
